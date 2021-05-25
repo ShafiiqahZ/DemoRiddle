e3f9c1b53e1b53ab3a1769603a2c2e673e128e0b
ShafiiqahZ <20037987@rp.edu.sg>
2021-05-25 11:52:04 +0800

    DemoRiddle

---
+++ app/src/main/java/sg/edu/rp/c346/id20037987/demoriddle/MainActivity.java
@@ -0,0 +1,48 @@
+package sg.edu.rp.c346.id20037987.demoriddle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Event handling Step 1
+    TextView tvQ1;
+    Button btnRevealQ1;
+    Button btnRevealQ2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Event handling Step 2
+        tvQ1 = findViewById(R.id.textViewQ1);
+        btnRevealQ1 = findViewById(R.id.buttonRevealQ1);
+        btnRevealQ2 = findViewById(R.id.buttonRevealQ2);
+
+        //Event handling Step 3
+        btnRevealQ1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AnswerActivity1.class);
+                intent.putExtra("Question", "Q1");
+                startActivity(intent);
+            }
+        });
+
+        btnRevealQ2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AnswerActivity2.class);
+                intent.putExtra("Question2", "Q2");
+                startActivity(intent);
+            }
+        });
+
+    }
+}
